a4e80ada0bf38ca572182f0e005acffe077246f1
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-05-31 16:12:30 +0530

    Update Single_Card_Creation.java

--- src/test/java/Smoke_Test/Single_Card_Creation.java
+++ src/test/java/Smoke_Test/Single_Card_Creation.java
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
